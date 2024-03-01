c398ee1d201e6a18f4df5751f1f5e21f3e36d133
LEGEND <legendboss123gaming@gmail.com>
2024-02-29 19:43:07 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ window.addEventListener('wheel', function (e) {
 
 
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ c });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -43,49 +43,81 @@ var angleY = 0;
 
 var heightmap = [];
 
-for (var y = 0; y < 500; y++) {
+for (var y = 0; y < 100; y++) {
     var arr = [];
-    for (var x = 0; x < 500; x++) {
-        if(Math.abs(x-250) + Math.abs(y-250)<30){
+    for (var x = 0; x < 100; x++) {
+        if (Math.abs(x - 50) + Math.abs(y - 50) < 30) {
             arr.push(0);
         }
-        else{
-            arr.push(100*(Math.sin(x) + Math.sin(y)));
+        else {
+            arr.push(100 * (Math.sin(x) + Math.sin(y)));
         }
     }
     heightmap.push(arr);
 }
-
+//heightmap = [[100, 0, 0, 0, 0], [0, 0, 0, 0, 0],[0, 0, 0, 0, 0], [0, 0, 0, 0, 0],[0, 0, 0, 0, 0]];
 var planeSize = {
     width: heightmap[0].length,
     height: heightmap.length
 };
 
+function getValue(x, y, map) {
+    if (x < 0 || x >= map[0].length || y < 0 || y >= map.length) {
+        return [false, 0];
+    }
+    return [true, map[y][x]];
+}
+
 function getHeight(x, y, map) {
-    var w = map[0].length;
-    var h = map.length;
+
+    if (x < 0 || x >= map[0].length || y < 0 || y >= map.length) {
+        return 0;
+    }
     var x1 = Math.floor(x);
     var x2 = x1 + 1;
     var y1 = Math.floor(y);
     var y2 = y1 + 1;
-    if (!inbound(0, w - 1, x1) || !inbound(0, w - 1, x2) || !inbound(0, h - 1, y1) || !inbound(0, h - 1, y2)) {
-        return 0;
+    var arr = [getValue(x1, y1, map), getValue(x2, y1, map), getValue(x1, y2, map), getValue(x2, y2, map)]
+    for (var i = 0; i < arr.length; i++) {
+        if (!arr[i][0]) {
+            return 0;
+        }
+        else {
+            arr[i] = arr[i][1];
+        }
     }
-    var lerp1 = lerp(map[y1][x1], map[y1][x2], x - x1);
-    var lerp2 = lerp(map[y2][x1], map[y2][x2], x - x1);
-    var final_height = lerp(lerp1, lerp2, y - y1);
-
+    var v0;
+    var v1;
+    var v2;
+    var point = { x: x, y: y };
+    if (x - x1 - y + y1 < 0) {
+        var v0 = { x: x2, y: y2, z: arr[3] };
+        var v1 = { x: x1, y: y2, z: arr[2] };
+        var v2 = { x: x1, y: y1, z: arr[0] };
+    }
+    else {
+        var v0 = { x: x1, y: y1, z: arr[0] };
+        var v1 = { x: x2, y: y2, z: arr[3] };
+        var v2 = { x: x2, y: y1, z: arr[1] };
+    }
+    var u = ((v1.y - v2.y) * (point.x - v2.x) + (v2.x - v1.x) * (point.y - v2.y)) /
+        ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y));
+    var v = ((v2.y - v0.y) * (point.x - v2.x) + (v0.x - v2.x) * (point.y - v2.y)) /
+        ((v1.y - v2.y) * (v0.x - v2.x) + (v2.x - v1.x) * (v0.y - v2.y));
+    var w = 1 - u - v;
+    var final_height = u * v0.z + v * v1.z + w * v2.z;
     return final_height;
 }
 
 
 
-size = { x: 80, y: 80 };
+size = { x: 100, y: 100 };
 
 var c = new THREE.PlaneGeometry(planeSize.width * size.x, planeSize.height * size.y, planeSize.width - 1, planeSize.height - 1);
 
 var pos = c.attributes.position;
-var flat_map = heightmap.flat();
+var flat_map = heightmap.slice().reverse().flat();
+
 for (var i = 0; i < pos.count; i++) {
     var x = i * 3;
     var y = i * 3 + 1;
@@ -94,9 +126,11 @@ for (var i = 0; i < pos.count; i++) {
 }
 c.computeVertexNormals();
 
-var m = new THREE.MeshPhongMaterial({ color: 0xFFFF00 });
+var m = new THREE.MeshPhongMaterial({ color: 0xFFFF00, transparent: true, opacity: 1 });
 var mesh = new THREE.Mesh(c, m);
 scene.add(mesh);
+mesh.position.add(new THREE.Vector3(size.x * planeSize.width / 2, size.y * planeSize.height / 2));
+
 var light = new THREE.DirectionalLight(0xFFFFFF, 1);
 light.position.set(0, 10, 5);
 light.target.position.set(-5, 0, 0);
@@ -105,16 +139,32 @@ scene.add(light.target);
 
 var light2 = new THREE.AmbientLight(0xFFFFFF, 0.5);
 scene.add(light2);
-var x = camera.position.x / size.x + planeSize.width / 2 - 0.5;
-var y = -camera.position.y / size.y + planeSize.height / 2 - 0.5;
-var d = 0.1;
+camera.position.x = 0;
+camera.position.y = 0;
+
+var x = camera.position.x * (planeSize.width - 1) / (planeSize.width * size.x);
+var y = camera.position.y * (planeSize.height - 1) / (planeSize.height * size.y);
+var d = 0.;
 var h = 5 + Math.max(getHeight(x + d, y, heightmap), getHeight(x - d, y, heightmap), getHeight(x, y + d, heightmap), getHeight(x, y - d, heightmap));
 
 camera.position.z = h;
 camera.rotation.x = 1;
 
 
-
+for (var y = -100; y <= 600; y += 10) {
+    for (var x = -100; x <= 600; x += 10) {
+        var s1 = new THREE.SphereGeometry(1, 10, 10);
+        var s2 = new THREE.MeshPhongMaterial({ color: 0xFF0000 });
+        var s3 = new THREE.Mesh(s1, s2);
+        s3.position.setX(x);
+        s3.position.setY(y);
+        var x1 = x * (planeSize.width - 1) / (planeSize.width * size.x);
+        var y1 = y * (planeSize.height - 1) / (planeSize.height * size.y);
+        var h = getHeight(x1, y1, heightmap);
+        s3.position.setZ(h);
+        scene.add(s3);
+    }
+}
 
 
 
@@ -143,14 +193,20 @@ function render() {
     }
     if (keysheld["Space"]) {
         if (onground) {
-            vel.z += 8;
+            vel.z += 5;
             onground = false;
         }
     }
+    if (keysheld["ShiftLeft"]) {
+        //if (onground) {
+        vel.z -= 5;
+        onground = false;
+        //}
+    }
 
-    var x = camera.position.x / size.x + planeSize.width / 2 - 0.5;
-    var y = -camera.position.y / size.y + planeSize.height / 2 - 0.5;
-    var h = 20 + Math.max(getHeight(x + d, y, heightmap), getHeight(x - d, y, heightmap), getHeight(x, y + d, heightmap), getHeight(x, y - d, heightmap));
+    var x = camera.position.x * (planeSize.width - 1) / (planeSize.width * size.x);
+    var y = camera.position.y * (planeSize.height - 1) / (planeSize.height * size.y);
+    var h = 5 + Math.max(getHeight(x + d, y, heightmap), getHeight(x - d, y, heightmap), getHeight(x, y + d, heightmap), getHeight(x, y - d, heightmap));
     vel.z += gravity;
     camera.position.add(vel);
     if (camera.position.z < h) {
@@ -159,10 +215,12 @@ function render() {
         if (vel.z != 0) {
             vel.z = 0;
         }
+        vel.z = 0;
     }
     else {
         onground = false;
     }
+    //vel.z = 0
     requestAnimationFrame(render);
 }
 render();
